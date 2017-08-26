9907c788ea3a9259e02888f213c4eb39435f0ded
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 08:26:20 +0800

    Fix typo

--- pysqueak/urls.py
+++ pysqueak/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from django.contrib import admin
-from worklogger2.views import api_root, ChoicesView
+from api.views import api_root, ChoicesView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
