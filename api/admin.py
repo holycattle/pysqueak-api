cc9ba69786d295b13ec1ffe3be0258e0cbae2604
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 15:27:44 +0000

    Configure admin site

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice, Answer
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        # put all other fields you want to be shown in listing
+        'version', 'title', 'created_on', 'updated_on',
+    )
+
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = (
+        # put all other fields you want to be shown in listing
+        'text', 'version',
+    )
+    raw_id_fields = ('question',)
+    def version(self, obj):
+        return obj.question.version
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = (
+        # put all other fields you want to be shown in listing
+        'choice_value', 'version',
+    )
+    raw_id_fields = ('question', 'choice',)
+    def version(self, obj):
+        return obj.question.version
+    def choice_value(self, obj):
+        return obj.choice.text
+
