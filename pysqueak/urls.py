636eb2c213a503f52b8e05876aa1e98bd05b9013
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 21:34:30 +0800

    Fix regex

--- pysqueak/urls.py
+++ pysqueak/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
         ChoicesView.as_view(), name='choices-get'),
     url(r'^answers/(?P<k>[0-9]+)/$',\
         AnswersView.as_view(), name='answers-post'),
-    url(r'^users/(?P<uuid>[0-9]+)/answers/(?P<ver>[0-9]+)/$',\
+    url(r'^users/(?P<uuid>([a-z]|[0-9])+)/answers/(?P<ver>[0-9]+)/$',\
         AnswersView.as_view(), name='latest-answers-post'),
 ]
