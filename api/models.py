924ed5e5a9e5d13a072fb6d3ad9759a756664d06
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 08:24:21 +0800

    Implement choices view

--- api/models.py
+++ api/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from rest_framework import serializers
 
-class Question(models.Model):
-    question = models.CharField(max_length=255)
+class Choice(models.Model):
+    text = models.CharField(max_length=255)
+    version = models.CharField(max_length=4)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+class Answer(models.Model):
+    choice_id = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    user_id = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('id', 'text', 'season', 'created_on', 'updated_on',)
+
+class AnswerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Answer
+        fields = ('id', 'choice_id', 'user_id', 'created_on',)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from api.models import Choice, Answer, ChoiceSerializer, AnswerSerializer
 
-# Create your views here.
+def get_not_implemented_message():
+    return {'code':200, 'data':['Not yet implemented']}
+
+# GET /
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response(get_not_implemented_message())
+
+class ChoicesView(APIView):
+    def get(self, request, id, format=None):
+        try:
+            choices = Choice.objects.filter(version=pk)
+            serializer = ChoiceSerializer(choices, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_403_FORBIDDEN)
