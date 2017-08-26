7b6d1d62fe22a8c643f3abba31a238a5651e4e6d
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 09:06:00 +0800

    Refactor models again

--- api/models.py
+++ api/models.py
@@ -3,25 +3,31 @@ from rest_framework import serializers
 
 class Question(models.Model):
     text = models.TextField()
-    version = models.CharField(max_length=4)
+    version = models.CharField(primary_key=True, max_length=8)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
 class Choice(models.Model):
-    text = models.CharField(max_length=255)
-    version = models.CharField(max_length=4)
+    text = models.TextField()
+    version = models.ForeignKey(Question, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
 class Answer(models.Model):
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-    user_id = models.CharField(max_length=255)
+    user_id = models.CharField()
     created_on = models.DateTimeField(auto_now_add=True)
 
 class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = ('id', 'text', 'season', 'created_on', 'updated_on',)
+
+class QuestionSerializer(serializers.ModelSerializer):
+    choices = ChoiceSerializer(many=True)
+    class Meta:
+        model = Question
+        fields('id', 'text', 'season', 'choices', 'created_on', 'updated_on',)
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
