4149411041384292e57feb69336bfa9e08766002
Joniel Ibasco <jlibasco@gmail.com>
2017-08-26 00:35:29 +0000

    Added CORS middleware; fixed settings.py

--- pysqueak/urls.py
+++ pysqueak/urls.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 from api.views import api_root, ChoicesView
 
 urlpatterns = [
+    url(r'^$', api_root),
     url(r'^admin/', admin.site.urls),
-    url(r'^tasks/mine/(?P<id>[0-9]+)/$',\
+    url(r'^choices/(?P<id>[0-9]+)/$',\
         ChoicesView.as_view(), name='choices-list'),
 ]
